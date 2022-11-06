44aefda76956e0e95962fe2d2b020a27894fcdf8
LuckyHead11 <jkkirby11@icloud.com>
2022-11-06 16:36:35 -0600

    new_item, and ordering

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 from os import path
@@ -9,9 +9,18 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///site.db"
 
 db= SQLAlchemy(app)
 
+class Item(db.Model):
+    id = id.Column(db.Integer, primary_key=True)
+    item_name = db.Column(db.String(100), nullable=False)
+    item_description = db.Column(db.String(255), nullable=False)
+    item_price = db.Column(db.String(100), nullable=False)
+
+
+
+
 @app.route('/')
 def index():
-    return "<h1>Hello World!</h1>"
+    return render_template('index.html')
 
 def create_database(app):
     if not path.exists("instance/" + "site.db"):
@@ -20,6 +29,17 @@ def create_database(app):
         print("Created Database!")
     else:
         print("Database already created!")
+
+@app.route('/order')
+def order():
+    return render_template('order.html')
+
+@app.route('/new_item')
+def new_item():
+    return render_template('new_item.html')
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
 if __name__ == '__main__':
     create_database(app)
     app.run(host="0.0.0.0", port=5000)
