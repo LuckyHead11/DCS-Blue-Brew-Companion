f0a73d8ee2f98bfc58fb978e855ad0cc826e6ea9
LuckyHead11 <jkkirby11@icloud.com>
2022-11-06 20:53:03 -0600

    More qol improvements

--- app.py
+++ app.py
@@ -9,12 +9,17 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///site.db"
 
 
 db= SQLAlchemy(app)
+current_items = []
+cost = 0
+
+
 
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     item_name = db.Column(db.String(100), nullable=False)
     item_description = db.Column(db.String(255), nullable=False)
     item_price = db.Column(db.String(100), nullable=False)
+    item_food = db.Column(db.Boolean, nullable=False, default=False)
 
 
 
@@ -33,8 +38,12 @@ def create_database(app):
 
 @app.route('/order')
 def order():
+    cost = 0
     items = Item.query.all()
-    return render_template('order.html', items=items)
+    for item in current_items:
+        cost += float(str(item.item_price).replace("$", ""))
+    print(cost)
+    return render_template('order.html', items=items, current_items=current_items,cost=cost)
 
 @app.route('/new_item')
 def new_item():
@@ -50,7 +59,12 @@ def add():
     item_name = request.form.get('item_name')
     item_description = request.form.get('item_description')
     item_price = request.form.get('item_price')
-    item = Item(item_name=item_name, item_description=item_description, item_price=item_price)
+    item_food = request.form.get('item_food')
+    if item_food == "on":
+        item_food = True
+    else:
+        item_food = False
+    item = Item(item_name=item_name, item_description=item_description, item_price=item_price, item_food=item_food)
 
     print(f"Item Name: {item_name} Item Description: {item_description} Item Price: {item_price}")
 
@@ -59,6 +73,11 @@ def add():
     db.session.commit()
     return redirect('/')
 
+@app.route('/add_order/<id>')
+def add_order(id):
+    item = Item.query.filter_by(id=id).first()
+    current_items.append(item)
+    return redirect('/order')
 
 if __name__ == '__main__':
     create_database(app)
